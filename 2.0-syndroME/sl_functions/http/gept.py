fa4a97bf0ac0aa430e1415e0b1f3fe7c90f97103
ibuninngu <31212444+ibuninngu@users.noreply.github.com>
2019-04-29 12:36:52 +0900

    Update gept.py

--- 2.0-syndroME/sl_functions/http/gept.py
+++ 2.0-syndroME/sl_functions/http/gept.py
@@ -1,6 +1,6 @@
 import sl_functions.http.actions_list as actions_list
 
-def post(req, arg, message_dir):
+def gept(req, arg, message_dir):
     try:
         content, content_type, p_status = actions_list.gept_list[req](arg)
         return(content, content_type, p_status)
